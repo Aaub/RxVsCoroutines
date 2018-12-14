9410555b581a37c7155f5b04f62f7110f498204b
nicolas_duponchel <nduponchel@oui.sncf>
2018-12-14 17:56:21 +0100

    Correctif factory

--- app/src/main/kotlin/com/alexetnico/rxvscoroutines/repo/BreweryApiServiceFactory.kt
+++ app/src/main/kotlin/com/alexetnico/rxvscoroutines/repo/BreweryApiServiceFactory.kt
@@ -3,22 +3,26 @@ package com.alexetnico.rxvscoroutines.repo
 import com.google.gson.GsonBuilder
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import com.alexetnico.rxvscoroutines.repo.coroutines.BreweryService as CoroutinesServices
 import com.alexetnico.rxvscoroutines.repo.rx.BreweryService as RxServices
 
 
 object BreweryApiServiceFactory {
-    fun createRxService(): RxServices = buildRetrofit()
+    fun createRxService(): RxServices = Retrofit.Builder()
+        .baseUrl(URL)
+        .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
+        .create(RxServices::class.java)
 
-    fun createCoroutinesService(): CoroutinesServices = buildRetrofit()
-
-    private inline fun <reified T> buildRetrofit() = Retrofit.Builder()
+    fun createCoroutinesService(): CoroutinesServices = Retrofit.Builder()
         .baseUrl(URL)
         .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
         .addCallAdapterFactory(CoroutineCallAdapterFactory())
         .build()
-        .create(T::class.java)
+        .create(CoroutinesServices::class.java)
 
     private const val URL = "https://sandbox-api.brewerydb.com/v2/"
 }
